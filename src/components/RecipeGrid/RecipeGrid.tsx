f623b4760f0fc911e6a25c1d5ea5ac66316f0c64
Gaetan <gaetan.gautier61@free.fr>
2026-04-16 00:24:08 +0200

    Ajout message si aucune recette

--- src/components/RecipeGrid/RecipeGrid.tsx
+++ src/components/RecipeGrid/RecipeGrid.tsx
@@ -1,16 +1,34 @@
 import { Recipe } from "@/src/types/Recipe";
 import RecipeCard from "@/src/components/RecipeCard/RecipeCard";
+import { useFilters } from "@/src/context/FilterContext";
 
 interface RecipeGridProps {
   recipes: Recipe[];
 }
 
 export default function RecipeGrid({ recipes }: RecipeGridProps) {
+
+  const { searchQuery } = useFilters();
+
+  if (recipes.length === 0 && searchQuery.length >= 3) {
+    return (
+      <section 
+        className="flex justify-center items-center" 
+        style={{ minHeight: "60vh" }}
+      >
+        <p className="text-gray-500 text-lg">
+          Aucune recette ne contient &quot;{searchQuery}&quot; vous pouvez chercher &quot;tarte aux pommes&quot;, &quot;poisson&quot; etc.
+        </p>
+      </section>
+    );
+  }
+
   return (
     <section
       className="flex justify-center pb-15"
       style={{ minHeight: "60vh" }}
     >
+      
       <div
         className="grid gap-[48px] grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center"
         style={{
